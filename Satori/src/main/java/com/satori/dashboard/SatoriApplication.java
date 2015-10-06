2909ece18c91bf12258b789aefcf76282f38c9a0
Eric Walden <Eric.Walden@1901group.com>
2015-10-06 14:45:26 -0400

    project set-up

---
+++ Satori/src/main/java/com/satori/dashboard/SatoriApplication.java
@@ -0,0 +1,12 @@
+package com.satori.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatoriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SatoriApplication.class, args);
+    }
+}
